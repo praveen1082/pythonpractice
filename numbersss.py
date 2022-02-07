10f58a0d7da33be6bbdef23afd7eedad7d838c1b
Prawin panta <praveenpanta1082@gmail.com>
2022-02-07 16:38:34 +0545

    Working with numbers

---
+++ numbersss.py
@@ -0,0 +1,32 @@
+# in python there are two types of numbers integers and floating point numbers
+
+print(10 ** 3)
+print(10 * 3)
+print(10/3)
+print(10//3)
+print(10%3)
+# augmented / enhanced assignment operator
+
+x = 10
+x += 3
+print(x)
+
+#operator precedence
+x = 10 + 3 * 2
+print(x)
+
+"""exponentiation,multiply or divide, add or subtract"""
+
+# using parenthesis provides a precendence
+#exercise by mosh
+# what is the output of x = (2+3)*10-3 =47
+x = (2+3)*10-3;
+print(x)
+
+#yahoo!! answer is correct
+
+x = -2.9
+print(round(x))
+print(abs(x))
+
+

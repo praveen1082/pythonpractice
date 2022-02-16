63e80d5eadcd0c2d99904044c6a4b4e5ce9489d1
Prawin panta <praveenpanta1082@gmail.com>
2022-02-16 12:55:12 +0545

    working with variables

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
-print("hello world")
+"""print("hello world")
 print("0----------```")
 print('   ||    || ')
+print('*'* 10)
+print('#'* 5)"""
 
+
+# understanding variables in python
+
+
+price = 10
+print(price)
+"""this prints the value of price in console"""

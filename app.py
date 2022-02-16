c6c70b071504f8834261eef41a80b5db77aff3fa
Prawin panta <praveenpanta1082@gmail.com>
2022-02-16 13:06:49 +0545

    Working with input and displaying them through concatenation

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ print('   ||    || ')
 print('*'* 10)
 print('#'* 5)"""
 
-
 # understanding variables in python
 
 
 price = 10
 print(price)
 """this prints the value of price in console"""
-
-
 
 price = 20
 print(price)
@@ -22,9 +19,20 @@ is_published = True
 print(is_published)
 is_published = False
 
-
 name = 'john smith'
 years = 20
 is_patient = False
 
+# receiving input from user
+# both inputs and print are the functions built-in in python
+# + operator is used to concatenation in python
 
+name = input("What is your name?: ")
+print('Hi ' + name)
+
+# Question by mosh: Ask two questions: person's name and favourite color. then, print a message like "Praveen Likes Blue"
+
+print("----------------------Solving the question above-------------")
+name = input("What is your name?: ")
+favourite_color = input("What is your favourite color?:")
+print(name+" likes "+favourite_color)

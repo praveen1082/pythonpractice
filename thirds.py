b4bf1d0957899e7a2bfd265ba880309e782eed27
Prawin panta <praveenpanta1082@gmail.com>
2022-02-18 15:59:06 +0545

    Solved the question from programming with mosh

---
+++ thirds.py
@@ -0,0 +1,8 @@
+# define a variable name and set it to jennifer and what is the output if print(name[1:-1])
+# without running program i am writting this output will be
+# ------------------------ ennife --------------------------------------------------------
+
+
+
+name = 'jennifer'
+print(name[1:-1])

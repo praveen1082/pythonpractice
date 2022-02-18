1653804ee8797b7c7725453d7297b97d8a8c2847
Prawin panta <praveenpanta1082@gmail.com>
2022-02-18 15:54:07 +0545

    Some workouts with strings in python

---
+++ strings.py
@@ -0,0 +1,15 @@
+course = 'python course for "begineers"'
+print(course)
+print(course[0])
+course = '''hi john 
+here is your first email
+thanks to the support'''
+print(course)
+print(course[-1])
+print(course[0:3])
+print(course[1:])
+
+#copying string to another string
+
+another = course[:]
+print(another)

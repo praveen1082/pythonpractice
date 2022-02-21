f15d50b52151f8f4eceadc732b2e476b7c53e543
Prawin panta <praveenpanta1082@gmail.com>
2022-02-21 16:01:02 +0545

    Solving issues in eoq

--- eoq.py
+++ eoq.py
@@ -1,27 +1,40 @@
-annual_requirement = input("please insert annual requirement / consumption / usage: ")
-order_cost = input("please insert order cost per unit: ")
-cost_per_unit = input("please insert cost per unit: ")
-percent_of_order_cost = input("please insert percentage of order cost per unit(like 10,20): ")
+import math
+annual_requirement = float(input("please insert annual requirement / consumption / usage: "))
+order_cost = float(input("please insert order cost per unit: "))
+cost_per_unit = float(input("please insert cost per unit: "))
+percent_of_order_cost = float(input("please insert percentage of order cost per unit(like 10,20): "))
 
 if annual_requirement is None:
     global user_press
-    user_press == input("press 1 for EOQ and 2 for Total cost at EOQ: ")
+    user_press == int(input("press 1 for EOQ and 2 for Total cost at EOQ: "))
     if user_press == 1:
         global eoq
-        eoq = input("Insert EOQ: ")
+        eoq = float(input("Insert EOQ: "))
     elif user_press == 2:
         global total_cost_eoq
-        total_cost_eoq = input("Insert Total cost at EOQ: ")
+        total_cost_eoq = float(input("Insert Total cost at EOQ: "))
     else:
         print("*******Either annual requirement / EOQ / Total cost at EOQ should be provided********")
+        print("______________________________________________EXITING________________________")
         quit()
 else:
     print("----------Calculating Carrying cost per order---------------")
     global carry_cost
-    carry_cost = (float(cost_per_unit)/100)*float(percent_of_order_cost)
+    costperunit = 1.0
+    if order_cost is None:
+        print("setting order cost default to Rs. 100")
+        ordercost = float(100)
+    else:
+        ordercost = float(order_cost)
+    if cost_per_unit is not None:
+        costperunit = float(cost_per_unit)
+
+    carry_cost = float((float(costperunit)/100.0)*float(percent_of_order_cost))
     print("Carrying cost: "+str(carry_cost))
     print("-----------Calculating EOQ---------------")
-    eoq =
+    eoq = math.sqrt((2*int(annual_requirement)*int(order_cost))/carry_cost)
+    print("value of EOQ is : "+str(eoq)+" units")
+
 
 
 
